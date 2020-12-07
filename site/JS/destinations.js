a9c46477410b66eaebb0b3c7e2de23800b280263
John John <fare.zagh@gmail.com>
2020-12-07 17:08:14 +0100

    oo

--- site/JS/destinations.js
+++ site/JS/destinations.js
@@ -35,17 +35,18 @@ function Recup()
 }
 
 function listHotels(hotels){
-    let paris= new destination(hotels[0].ville , hotels[0].prixAdulte , hotels[0].petitDej , hotels[0].animauxOK, hotels[0].image,hotels[0].continent, hotels[0].lien, listeTemp[0]);
-    let londres= new destination(hotels[1].ville , hotels[1].prixAdulte , hotels[1].petitDej , hotels[1].animauxOK, hotels[1].image,hotels[1].continent, hotels[1].lien, listeTemp[1]);
-    let newYork= new destination(hotels[2].ville , hotels[2].prixAdulte , hotels[2].petitDej , hotels[2].animauxOK, hotels[2].image,hotels[2].continent, hotels[2].lien, listeTemp[2]);
-    let venice= new destination(hotels[3].ville , hotels[3].prixAdulte , hotels[3].petitDej , hotels[3].animauxOK, hotels[3].image,hotels[3].continent, hotels[3].lien, listeTemp[3]);
-    let johannesburg= new destination(hotels[4].ville , hotels[4].prixAdulte , hotels[4].petitDej , hotels[4].animauxOK, hotels[4].image,hotels[4].continent, hotels[4].lien, listeTemp[4]);
-    let LasVegas= new destination(hotels[5].ville , hotels[5].prixAdulte , hotels[5].petitDej , hotels[5].animauxOK, hotels[5].image,hotels[5].continent, hotels[5].lien, listeTemp[5]);
-    let singapour= new destination(hotels[6].ville , hotels[6].prixAdulte , hotels[6].petitDej , hotels[6].animauxOK, hotels[6].image,hotels[6].continent, hotels[6].lien, listeTemp[6]);
-    let Rio= new destination(hotels[7].ville , hotels[7].prixAdulte , hotels[7].petitDej , hotels[7].animauxOK, hotels[7].image,hotels[7].continent, hotels[7].lien, listeTemp[7]);
-    let sydney= new destination(hotels[8].ville , hotels[8].prixAdulte , hotels[8].petitDej , hotels[8].animauxOK, hotels[8].image,hotels[8].continent, hotels[8].lien, listeTemp[8]);
-    let tokyo= new destination(hotels[9].ville , hotels[9].prixAdulte , hotels[9].petitDej , hotels[9].animauxOK, hotels[9].image,hotels[9].continent, hotels[9].lien, listeTemp[9]);
-    listeHotels=[paris,londres,newYork,venice,johannesburg,LasVegas,singapour,Rio,sydney,tokyo];
+    //let paris= new destination(hotels[0].ville , hotels[0].prixAdulte , hotels[0].petitDej , hotels[0].animauxOK, hotels[0].image,hotels[0].continent, hotels[0].lien, listeTemp[0]);
+    //let londres= new destination(hotels[1].ville , hotels[1].prixAdulte , hotels[1].petitDej , hotels[1].animauxOK, hotels[1].image,hotels[1].continent, hotels[1].lien, listeTemp[1]);
+    //let newYork= new destination(hotels[2].ville , hotels[2].prixAdulte , hotels[2].petitDej , hotels[2].animauxOK, hotels[2].image,hotels[2].continent, hotels[2].lien, listeTemp[2]);
+    //let venice= new destination(hotels[3].ville , hotels[3].prixAdulte , hotels[3].petitDej , hotels[3].animauxOK, hotels[3].image,hotels[3].continent, hotels[3].lien, listeTemp[3]);
+    //let johannesburg= new destination(hotels[4].ville , hotels[4].prixAdulte , hotels[4].petitDej , hotels[4].animauxOK, hotels[4].image,hotels[4].continent, hotels[4].lien, listeTemp[4]);
+    //let LasVegas= new destination(hotels[5].ville , hotels[5].prixAdulte , hotels[5].petitDej , hotels[5].animauxOK, hotels[5].image,hotels[5].continent, hotels[5].lien, listeTemp[5]);
+    //let singapour= new destination(hotels[6].ville , hotels[6].prixAdulte , hotels[6].petitDej , hotels[6].animauxOK, hotels[6].image,hotels[6].continent, hotels[6].lien, listeTemp[6]);
+    //let Rio= new destination(hotels[7].ville , hotels[7].prixAdulte , hotels[7].petitDej , hotels[7].animauxOK, hotels[7].image,hotels[7].continent, hotels[7].lien, listeTemp[7]);
+    //let sydney= new destination(hotels[8].ville , hotels[8].prixAdulte , hotels[8].petitDej , hotels[8].animauxOK, hotels[8].image,hotels[8].continent, hotels[8].lien, listeTemp[8]);
+    //let tokyo= new destination(hotels[9].ville , hotels[9].prixAdulte , hotels[9].petitDej , hotels[9].animauxOK, hotels[9].image,hotels[9].continent, hotels[9].lien, listeTemp[9]);
+    //listeHotels=[paris,londres,newYork,venice,johannesburg,LasVegas,singapour,Rio,sydney,tokyo];
+    listeHotels=hotels;
     console.log(listeHotels)
     addDestination(listeHotels)
 }    
@@ -172,9 +173,9 @@ function remontrer() {
  }
  function Temperature(listeVille)
 {
-    valren= []
+    
     for(ville of listeVille){
-        console.log(ville)
+        
         let appid="bfb725a2d2eb425c0443cbcdf5c91e8f";
         fetch("https://api.openweathermap.org/data/2.5/weather?q="+ville+"&appid="+appid+"&units=metric").then(function(reponse)
         {
@@ -184,11 +185,12 @@ function remontrer() {
         .then(function(json)
         {   
             console.log(json["main"]["temp"])
-            valren.push(json["main"]["temp"])
+            listeTemp.push(json["main"]["temp"])
         })
+        
 
     }
-    return valren
+    console.log(listeTemp)
     
     
 }
@@ -200,10 +202,12 @@ function conti(){
     filtre()
 
 }
-
+var listeTemp=[] ;
+Temperature(["Paris","Londres","New York","Venice","Johannesburg","Las Vegas","Singapour", "Rio","Sydney","Tokyo"])
 var listeHotels;
 var i=0;
 var compteur;
 Recup();
-var listeTemp =Temperature(["Paris","Londres","New York","Venice","Johannesburg","Las Vegas","Singapour", "Rio","Sydney","Tokyo"]) 
+
+ 
 window.onscroll=function(){scroll()};
